939c322f412f9fa88df7427c48786ffbde3f7950
test <test@MacBook-Pro-test.local>
2023-01-17 16:39:56 +0100

    production

--- server/index.js
+++ server/index.js
@@ -18,6 +18,14 @@ app.use('/api', router)
 app.use('/t', RedirectRouter)
 app.use(errorsMiddleware)
 
+if (process.env.NODE_ENV === 'production') {
+    app.use('/', express.static(path.join(__dirname, 'client', 'build')))
+  
+    app.get('*', (req, res) => {
+      res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+    })
+  }
+
 const PORT = config.get('port')  || 5000
 
 async function start(){
